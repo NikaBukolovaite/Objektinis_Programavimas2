d850b46fec44b67932eb43adcc70742d766e3241
Nika <bukolovaite.nika@gmail.com>
2025-02-10 15:30:40 +0200

    padaryta v.pradine

--- main.cpp
+++ main.cpp
@@ -5,37 +5,47 @@ struct Studentas
 	string vardas;
 	string pavarde;
 	vector<int> pazymiai;
-	int egzamino_pazymys;
+	int egzamino_pazymys = 0;
 	double vidurkis;
 };
 
 void informacijos_ivedimas(vector<Studentas> &studentai)
 {
-	int studentu_skaicius;
-	int pazymiu_skaicius;
+	int studentu_skaicius = 0;
 	cout << "Iveskite kiek mokiniu norite ivesti: ";
-	cin >> studentu_skaicius;
-
+	while (studentu_skaicius <= 0)
+	{
+		cin >> studentu_skaicius;
+	}
 	for (int i = 0; i < studentu_skaicius; i++)
 	{
+		int pazymiu_skaicius = 0;
 		Studentas laikinas;
 		cout << "Iveskite studento varda: ";
 		cin >> laikinas.vardas;
 		cout << "Iveskite studento pavarde: ";
 		cin >> laikinas.pavarde;
 		cout << "Iveskite kiek viso yra pazymiu: ";
-		cin >> pazymiu_skaicius;
-
+		while (pazymiu_skaicius <= 0)
+		{
+			cin >> pazymiu_skaicius;
+		}
 		for (int j = 0; j < pazymiu_skaicius; j++)
 		{
-			int pazymys;
-			cout << "Iveskite pazymius: ";
-			cin >> pazymys;
+			int pazymys = 0;
+			cout << "Iveskite " << j + 1 << " pazymi: ";
+			while (pazymys < 1 || pazymys > 10)
+			{
+				cin >> pazymys;
+			}
 			laikinas.pazymiai.push_back(pazymys);
 		}
 
 		cout << "Iveskite egzamino pazymi: ";
-		cin >> laikinas.egzamino_pazymys;
+		while (laikinas.egzamino_pazymys < 1 || laikinas.egzamino_pazymys > 10)
+		{
+			cin >> laikinas.egzamino_pazymys;
+		}
 		studentai.push_back(laikinas);
 	}
 }
@@ -60,7 +70,7 @@ double galutinis_pazymys_med(Studentas studentas)
 
 	if (studentas.pazymiai.size() % 2 == 0)
 	{
-		mediana = (studentas.pazymiai[studentas.pazymiai.size() / 2 - 1] + studentas.pazymiai[studentas.pazymiai.size() / 2 + 1]);
+		mediana = (studentas.pazymiai[studentas.pazymiai.size() / 2 - 1] + studentas.pazymiai[studentas.pazymiai.size() / 2 + 1]) / 2;
 	}
 	else
 	{
@@ -72,33 +82,48 @@ double galutinis_pazymys_med(Studentas studentas)
 
 void output(vector<Studentas> studentai)
 {
+	string skaiciavimo_budas;
+	cout << "Iveskite kaip norite apskaiciuoti galutini pazymi: (Vidurkis/ Mediana): ";
+	while (skaiciavimo_budas != "Vidurkis" && skaiciavimo_budas != "Mediana")
+	{
+		if (skaiciavimo_budas != "Vidurkis" && skaiciavimo_budas != "Mediana")
+		{
+			cin >> skaiciavimo_budas;
+		}
+		else
+		{
+			cout << "Ivestas neteisingas skaiciavimo budas. Iveskite dar karta kaip norite apskaiciuoti galutini pazymi: (Vidurkis/ Mediana): ";
+		}
+	}
+
+	cout << setw(15) << left << "Vardas" << setw(15) << left << "Pavarde";
+	if (skaiciavimo_budas == "Vidurkis")
+	{
+		cout << setw(15) << left << "Galutinis (Vid.): " << endl;
+	}
+	else if (skaiciavimo_budas == "Mediana")
+	{
+		cout << setw(15) << left << "Galutinis (Med.): " << endl;
+	}
+	cout << "--------------------------------------------------------------------------------------\n";
+
 	for (int i = 0; i < studentai.size(); i++)
 	{
-		cout << "Pavarde: " << studentai[i].pavarde << endl;
-		cout << "Vardas: " << studentai[i].vardas << endl;
-		for (int j = 0; j < studentai[i].pazymiai.size(); j++)
-		{
-			cout << "Pazymiai: " << studentai[i].pazymiai[j] << endl;
-		}
-		cout << "Egzamino pazymys: " << studentai[i].egzamino_pazymys << endl;
-
-		string skaiciavimo_budas;
-		cout << "Iveskite kaip norite apskaiciuoti galutini pazymi: (Vidurkis/ Mediana): ";
-		cin >> skaiciavimo_budas;
+		cout << setw(15) << left << studentai[i].vardas << setw(15) << left << studentai[i].pavarde;
 		if (skaiciavimo_budas == "Vidurkis")
 		{
-			cout << "Galutinis (Vid.): " << galutinis_pazymys_vid(studentai[i]) << endl;
+			cout << setw(15) << left << setprecision(3) << galutinis_pazymys_vid(studentai[i]) << endl;
 		}
 		else if (skaiciavimo_budas == "Mediana")
 		{
-			cout << "Galutinis (Med.): " << galutinis_pazymys_med(studentai[i]) << endl;
+			cout << setw(15) << left << setprecision(3) << galutinis_pazymys_med(studentai[i]) << endl;
 		}
 	}
 }
 
 int main()
 {
-	// pradineje paprasyti, kad zmogus parasytu varda, pabvarde, pazymius ir paimtu is terminalo ta info.
 	vector<Studentas> studentai;
 	informacijos_ivedimas(studentai);
+	output(studentai);
 }
--- santrauka.h
+++ santrauka.h
@@ -3,11 +3,17 @@
 #include <string>
 #include <fstream>
 #include <algorithm>
+#include <iomanip>
+#include <sstream>
 
 // using std::
 using std::cin;
 using std::cout;
 using std::endl;
+using std::left;
+using std::right;
+using std::setprecision;
+using std::setw;
 using std::sort;
 using std::string;
 using std::vector;
