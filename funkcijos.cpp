7fb28581983aee0ef8b46aa9723c8d6eca42a30f
Nika <bukolovaite.nika@gmail.com>
2025-03-02 16:02:20 +0200

    optimizuotas kodas

--- funkcijos.cpp
+++ funkcijos.cpp
@@ -149,34 +149,119 @@ void pabaiga(vector<std::chrono::duration<double>> testuLaikai, int testuSkaiciu
 	}
 }
 
+bool ivestiStudenta(Studentas &laikinas)
+{
+	cout << "Iveskite studento varda ir pavarde (Jei norite baigti, irasykite 'n'.): ";
+	cin >> laikinas.vardas;
+	if (laikinas.vardas == "n")
+		return false;
+	cin >> laikinas.pavarde;
+	return true;
+}
+
+void ivestiPazymius(Studentas &laikinas)
+{
+	int pazymys = 0;
+	cout << "Iveskite studento namu darbu pazymius (Jei norite baigti pazymiu irasyma irasykite -1.): ";
+
+	while (true)
+	{
+		try
+		{
+			cin >> pazymys;
+			if (pazymys == -1)
+				break;
+			if (pazymys < 1 || pazymys > 10 || cin.fail())
+			{
+				throw std::invalid_argument("Ivedete netinkama simboli. Iveskite dar karta: ");
+			}
+			laikinas.pazymiai.push_back(pazymys);
+		}
+		catch (std::exception &e)
+		{
+			cout << e.what() << endl;
+			cin.clear();
+			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		}
+	}
+
+	cout << "Iveskite egzamino pazymi: ";
+	while (true)
+	{
+		try
+		{
+			cin >> laikinas.egzamino_pazymys;
+			if (laikinas.egzamino_pazymys >= 1 && laikinas.egzamino_pazymys <= 10)
+				break;
+			throw std::invalid_argument("Ivedete netinkama simboli. Iveskite dar karta: ");
+		}
+		catch (std::exception &e)
+		{
+			cout << e.what() << endl;
+			cin.clear();
+			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		}
+	}
+}
+
+void generuotiPazymius(Studentas &laikinas)
+{
+	int pazymiu_kiekis = 0;
+	cout << "Iveskite kiek pazymiu norite sugeneruoti: ";
+	while (pazymiu_kiekis <= 0 || pazymiu_kiekis > 1000)
+	{
+		try
+		{
+			cin >> pazymiu_kiekis;
+			if (pazymiu_kiekis <= 0 || pazymiu_kiekis > 1000 || cin.fail())
+			{
+				throw std::invalid_argument("Ivedete netinkama simboli. Iveskite dar karta: ");
+			}
+		}
+		catch (std::exception &e)
+		{
+			cout << e.what() << endl;
+			cin.clear();
+			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		}
+	}
+
+	cout << "Sugeneruoti pazymiai: ";
+	for (int i = 0; i < pazymiu_kiekis; i++)
+	{
+		int pazymys = rand() % 10 + 1;
+		cout << pazymys << " ";
+		laikinas.pazymiai.push_back(pazymys);
+	}
+	cout << endl;
+
+	laikinas.egzamino_pazymys = rand() % 10 + 1;
+	cout << "Egzamino pazymys: " << laikinas.egzamino_pazymys << endl;
+}
+
 void informacijos_ivedimas(vector<Studentas> &studentai, int pasirinkimas, string failo_pasirinkimas)
 {
 	while (true)
 	{
 		Studentas laikinas;
-		if (pasirinkimas == 4)
+		string random_vardas[10] = {"Marija", "Arnas", "Tomas", "Greta", "Gabija", "Paulius", "Lukas", "Egle", "Rokas", "Ieva"};
+		string random_pavarde[10] = {"Petrauskaite", "Petrauskas", "Tomauskas", "Gretauskaite", "Gabijauskaite", "Paulauskas", "Lukauskaite", "Matuolis", "Rokauskas", "Ievauskaite"};
+		int generavimas = 0;
+		switch (pasirinkimas)
 		{
-			failo_nuskaitymas(studentai, failo_pasirinkimas);
-			return;
-		}
-		else if (pasirinkimas == 1 || pasirinkimas == 2)
-		{
-			cout << "Iveskite studento varda ir pavarde (Jei norite baigti irasyma irasykite 'n'.): ";
-			cin >> laikinas.vardas;
-			if (laikinas.vardas == "n")
-			{
+		case 1:
+			if (!ivestiStudenta(laikinas))
 				return;
-			}
-			else
-			{
-				cin >> laikinas.pavarde;
-			}
-		}
-		else if (pasirinkimas == 3)
-		{
-			string random_vardas[10] = {"Marija", "Arnas", "Tomas", "Greta", "Gabija", "Paulius", "Lukas", "Egle", "Rokas", "Ieva"};
-			string random_pavarde[10] = {"Petrauskaite", "Petrauskas", "Tomauskas", "Gretauskaite", "Gabijauskaite", "Paulauskas", "Lukauskaite", "Matuolis", "Rokauskas", "Ievauskaite"};
-			int generavimas = 0;
+			ivestiPazymius(laikinas);
+			break;
+
+		case 2:
+			if (!ivestiStudenta(laikinas))
+				return;
+			generuotiPazymius(laikinas);
+			break;
+
+		case 3:
 			cout << "Jei norite sugeneruoti varda ir pavarde irasykite 1, jei norite uzbaigti programa irasykite 2: ";
 			while (generavimas != 1 && generavimas != 2)
 			{
@@ -204,90 +289,16 @@ void informacijos_ivedimas(vector<Studentas> &studentai, int pasirinkimas, strin
 					cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 				}
 			}
-		}
-		if (pasirinkimas == 1)
-		{
-			cout << "Iveskite studento namu darbu pazymius (Jei norite baigti pazymiu irasyma irasykite -1.): ";
-			int pazymys = 0;
-			while (pazymys != -1)
-			{
-				try
-				{
-					cin >> pazymys;
+			generuotiPazymius(laikinas);
+			break;
 
-					if (pazymys == -1)
-					{
-						break;
-					}
-					else if (pazymys <= 0 || pazymys > 10 || cin.fail())
-					{
-						throw std::invalid_argument("Ivedete netinkama simboli. Iveskite dar karta: ");
-					}
-					else
-					{
-						laikinas.pazymiai.push_back(pazymys);
-					}
-				}
-				catch (std::exception &e)
-				{
-					cout << e.what() << endl;
-					cin.clear();
-					cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-				}
-			}
+		case 4:
+			failo_nuskaitymas(studentai, failo_pasirinkimas);
+			return;
 
-			cout << "Iveskite egzamino pazymi: ";
-			laikinas.egzamino_pazymys = 0;
-			while (laikinas.egzamino_pazymys < 1 || laikinas.egzamino_pazymys > 10 || cin.fail())
-			{
-				try
-				{
-					cin >> laikinas.egzamino_pazymys;
-					if (laikinas.egzamino_pazymys < 1 || laikinas.egzamino_pazymys > 10 || cin.fail())
-					{
-						throw std::invalid_argument("Ivedete netinkama simboli. Iveskite dar karta: ");
-					}
-				}
-				catch (std::exception &e)
-				{
-					cout << e.what() << endl;
-					cin.clear();
-					cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-				}
-			}
-		}
-		else if (pasirinkimas == 2 || pasirinkimas == 3)
-		{
-			int pazymiu_kiekis = 0;
-			int pazymys = 0;
-			cout << "Iveskite kiek pazymiu norite sugeneruoti: ";
-			while (pazymiu_kiekis <= 0 || pazymiu_kiekis > 1000)
-			{
-				try
-				{
-					cin >> pazymiu_kiekis;
-					if (pazymiu_kiekis <= 0 || pazymiu_kiekis > 100 || cin.fail())
-					{
-						throw std::invalid_argument("Ivedete netinkama simboli. Iveskite dar karta: ");
-					}
-				}
-				catch (std::exception &e)
-				{
-					cout << e.what() << endl;
-					cin.clear();
-					cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-				}
-			}
-			for (int i = 0; i < pazymiu_kiekis; i++)
-			{
-				int pazymys = rand() % 10 + 1;
-				cout << pazymys << " ";
-				laikinas.pazymiai.push_back(pazymys);
-			}
-			cout << endl;
-			cout << "Egzamino pazymys: ";
-			laikinas.egzamino_pazymys = rand() % 10 + 1;
-			cout << laikinas.egzamino_pazymys << endl;
+		default:
+			cout << "Neteisingas pasirinkimas. Bandykite dar kartą." << endl;
+			return;
 		}
 
 		studentai.push_back(laikinas);
